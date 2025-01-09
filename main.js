b1001aa3d582dc1320d2084d783ba422a65d7413
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-01-09 17:10:34 +0100

    Add variables

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-let
-let
+let età = prompt("Salva, prego l'inserire l'età per favorire della promozione")
+let kmtr = prompt("Inserisca i chilometri della tratta")
+const price = 0.21
+
+console.log(età, kmtr, price)
+
+
+
+
