47a8f4daf9e943d86003ff022fb23ce93fe7f065
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-01-09 18:28:55 +0100

    Add: small fixes

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// VARIABILI E COSTANTI
+
 let età = prompt("Salve, prego inserire l'età per favorire della promozione");
 let kmtr = prompt("Inserisca i chilometri della tratta");
 const sc1 = 0.80;
@@ -6,24 +8,27 @@ const price = 0.21;
 
 console.log(parseInt(età), parseInt(kmtr), price)
 
+// PRIMA OPERAZIONE
+
 let start = price * kmtr
 
 console.log( `Il prezzo base é ${start.toFixed(2)}€` )
 
+// CONDIZIONI
 
 if ( età < 18 ){
 
-    let finale = start * sc1;
+    let final = start * sc1;
 
-    console.log ( `Il prezzo per la tua età é ${finale.toFixed(2)}€` )
+    console.log ( `Il prezzo per la tua età é ${final.toFixed(2)}€` )
 
 }
 
 if ( età > 65 ) {
 
-    let finale = start * sc2
+    let final = start * sc2
 
-    console.log ( `Il prezzo per la tua età é ${finale.toFixed(2)}€` )
+    console.log ( `Il prezzo per la tua età é ${final.toFixed(2)}€` )
 
 }
 
