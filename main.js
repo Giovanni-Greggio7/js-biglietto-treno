acc03185a7bf2083049a7d05b292c943533d0c81
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-01-09 17:33:16 +0100

    Variables console fixed

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-let età = prompt("Salva, prego l'inserire l'età per favorire della promozione")
+let età = prompt("Salve, prego inserire l'età per favorire della promozione")
 let kmtr = prompt("Inserisca i chilometri della tratta")
 const price = 0.21
 
-console.log(età, kmtr, price)
+console.log(parseInt(età), parseInt(kmtr), price)
+
 
 
 
